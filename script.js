f213d13c33725fcd667792a04a304fd757901220
itshav4 <fahmi.mulkishava@gmail.com>
2024-10-17 21:08:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -51,8 +51,7 @@ function acceptConfession() {
     
     acceptMessage.innerHTML = `
         Terima kasih telah menerima perasaanku...<br><br>
-        Aku berjanji akan membuatmu bahagia setiap hari, karena kamu adalah segalanya bagiku.<br><br>
-        Bersamamu adalah impian yang ingin kujalani selamanya. 💖💐💐
+        Aku akan membuatmu gembira setiap hari, karena kamu adalah segalanya bagiku 💖💐💐.<br><br> 
     `;
     acceptMessage.classList.add("revealed");
     acceptButton.style.display = "none";  // Menghilangkan tombol setelah menerima confession
