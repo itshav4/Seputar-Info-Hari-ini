3a3558130724c26b86c2103cb06caaba163b0036
itshav4 <fahmi.mulkishava@gmail.com>
2024-10-17 20:20:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,8 @@ function nextStep() {
         revealButton.style.display = "none";  // Menghilangkan tombol setelah klik terakhir
         message.innerHTML = `
             Aku mencintaimu...<br><br>
-            Sejak hari pertama aku mengenalmu, aku tahu ada sesuatu yang istimewa tentangmu.<br><br>
+            Sejak pertama kali aku melihat kamu saat kamu nonton turnamen dan aku yang saat itu bermain melawan sekolah kamu.<br><br>
+            Di saat pertama kali melihat kamu aku langsung jatuh hati, dan beruntung nyaa aku saat MPLS aku melihat kamu dan hati rasanya ingin mengatakan aku cinta.<br><br>
             Cara kamu tersenyum, cara kamu bicara, setiap detik yang kuhabiskan denganmu terasa seperti kebahagiaan yang tak tergambarkan.<br><br>
             Aku tak bisa lagi menahan perasaan ini, jadi aku harus memberitahumu...<br><br>
             Kamu adalah alasan aku tersenyum setiap hari. Kamu adalah orang yang selalu ada dalam pikiranku.<br><br>
